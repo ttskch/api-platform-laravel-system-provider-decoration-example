ca0438ab93dbd4dd0a2d31b0ee239747b67635e4
Takashi Kanemoto <ttskch@gmail.com>
2025-10-18 10:11:59 +0900

    fix: 🐛 eliminate circular dependency

--- app/State/AppWriteProcessor.php
+++ app/State/AppWriteProcessor.php
@@ -11,12 +11,14 @@ use ApiPlatform\State\ProcessorInterface;
 final class AppWriteProcessor implements ProcessorInterface
 {
     public function __construct(
-        private readonly WriteProcessor $decorated,
+        private readonly mixed $decorated,
     ) {
     }
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
+        assert($this->decorated instanceof WriteProcessor);
+        
         return $this->decorated->process($data, $operation, $uriVariables, $context);
     }
 }
